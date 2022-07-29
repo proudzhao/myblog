c92087fb65077b02080750389373dbba3dcccfa3
zhihao zhao <1973916976@qq.com>
2022-07-29 20:49:27 +0800

    create article app

---
+++ article/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/7/29 20:41
+# @Author : proudzhao
+from django.urls import path
+
+app_name = 'artile'  # 正在部署的应用名称
+
+urlpatterns = [
+
+]
--- myblog/settings.py
+++ myblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'article',  # 激活app
 ]
 
 MIDDLEWARE = [
