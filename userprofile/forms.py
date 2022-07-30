3f7d87ac6099e1bd32a8841942666dbc5604d251
zhihao zhao <1973916976@qq.com>
2022-07-30 15:18:44 +0800

    add register function

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -8,3 +8,21 @@ from django.contrib.auth.models import User
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    # 对两次输入的密码检查是否一致
+    # def clean_[字段]这种写法Django会自动调用，来对单个字段的数据进行验证清洗
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data.get('password') == data.get('password2'):
+            return data.get('password')
+        else:
+            raise forms.ValidationError("密码输入不一致，请重试。")
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -10,4 +10,5 @@ app_name = 'userprofile'
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
