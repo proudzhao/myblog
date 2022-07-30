674df68b10ed98ba78f01358687c96fe0e0ce5bf
zhihao zhao <1973916976@qq.com>
2022-07-30 09:55:37 +0800

    add some templates

--- article/views.py
+++ article/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from article.models import ArticlePost
+from django.shortcuts import render
 
 
 def article_list(request):
-    return HttpResponse("Hello World!")
+    # 取出所有博客文章
+    articles = ArticlePost.objects.all()
+    # 需要传递给模版(templates)的对象
+    context = {'articles': articles}
+    # render 函数: 载入模版，并返回context对象
+    return render(request, 'article/list.html', context)
